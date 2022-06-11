442e3bc6c055f1809f5b11f2a9fabfaaf2b3b40f
Quadjovie Antonio <quadjovie.antonio@gmail.com>
2022-06-12 00:09:29 +0200

    scroll top

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const btnUp = document.querySelector('.btnUp');
 
+// scroller vers le haut
 btnUp.addEventListener('click', () =>{
 
     window.scrollTo({
@@ -9,6 +10,7 @@ btnUp.addEventListener('click', () =>{
     })
 })
 
+// slid du document
 const slidingNewsletter = document.querySelector('.slide-in');
 
 window.addEventListener('scroll', () => {
@@ -30,7 +32,7 @@ window.addEventListener('scroll', () => {
 
 let date1 = new Date();
 
-//---------------------- parametre de date
+// parametre de date
 
 let dateLocale = date1.toLocaleString('fr-FR',{
     weekday: 'long',
@@ -44,3 +46,31 @@ let dateLocale = date1.toLocaleString('fr-FR',{
 document.getElementById('p1').innerHTML = ' ' + dateLocale;
 
 p1.style.marginTop  = "20px";
+
+// scroll indicator
+
+const scrollIndicator = document.querySelector(".scroll-indicator");
+const main = document.querySelector("main");
+
+const observer = new IntersectionObserver(handleIntersect)
+
+observer.observe(main)
+
+function handleIntersect(entries){
+    const el = entries[0]; 
+    if(el.isIntersecting){
+        window.addEventListener("scroll", indicatorAnimation)
+    } else if(!el.isIntersecting) {
+        window.addEventListener("scroll", indicatorAnimation)
+    }
+}
+
+function indicatorAnimation() {
+    if(window.scrollY > main.offsetTop) {
+        const percentage = ((window.scrollY - main.offsetTop) / 
+        main.scrollHeight * 100).toFixed(2);
+        scrollIndicator.style.transform = `scaleX(${(percentage / 100)})`
+    } else{
+        scrollIndicator.style.transform = "scaleX(0)"
+    }
+}
